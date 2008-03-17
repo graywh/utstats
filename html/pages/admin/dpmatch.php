b58147733ef8f98b1d12d1baaeda8a891bc796d5
Will Gray <graywh@gmail.com>
2008-03-17 14:19:23 -0500

    Fixed PHP+SQL syntax errors.

--- html/pages/admin/dpmatch.php
+++ html/pages/admin/dpmatch.php
@@ -64,7 +64,7 @@ echo'</tr>
 	$q_match = mysql_query("SELECT matchid, playerid FROM uts_player WHERE pid = '$pid' and matchid = '$matchid'") or die(mysql_error());
 	while ($r_match = mysql_fetch_array($q_match))
 	{
-		mysql_query("DELETE FROM uts_killsmatrix WHERE matchid = '$r_match['matchid']' AND (killer = '$r_match['playerid']' OR victim = '$r_match['playerid']')") or die(mysql_error());
+		mysql_query("DELETE FROM uts_killsmatrix WHERE matchid = '${r_match['matchid']}' AND (killer = '${r_match['playerid']}' OR victim = '${r_match['playerid']}')") or die(mysql_error());
 	}
 	echo'<td class="grey" align="left">Done</td>
 </tr>
@@ -87,7 +87,7 @@ mysql_query("DELETE FROM uts_weaponstats WHERE matchid IN ('$matchid','0') AND p
 $q_weaponstats = mysql_query("SELECT SUM(kills) AS kills, SUM(shots) AS shots, SUM(hits) as hits, SUM(damage) as damage, LEAST(ROUND(10000*SUM(hits)/SUM(shots))/100,100) AS acc FROM uts_weaponstats WHERE pid = '$pid'  GROUP BY weapon") or die(mysql_error());
 while ($r_weaponstats = mysql_fetch_array($q_weaponstats))
 {
-	mysql_query("INSERT INTO uts_weaponstats SET matchid='0', pid='$pid', kills='$r_weaponstats['kills']', shots='$r_weaponstats['shots']', hits='$r_weaponstats['hits']', damage='$r_weaponstats['damage']', acc='$r_weaponstats['acc']'") or die(mysql_error());
+	mysql_query("INSERT INTO uts_weaponstats SET matchid='0', pid='$pid', kills='${r_weaponstats['kills']}', shots='${r_weaponstats['shots']}', hits='${r_weaponstats['hits']}', damage='${r_weaponstats['damage']}', acc='${r_weaponstats['acc']}'") or die(mysql_error());
 }
 	echo'<td class="grey" align="left" width="400">Done</td>
 </tr>
@@ -98,7 +98,7 @@ while ($r_weaponstats = mysql_fetch_array($q_weaponstats))
 	$q_weaponstats = mysql_query("SELECT weapon, SUM(kills) AS kills, SUM(shots) AS shots, SUM(hits) as hits, SUM(damage) as damage, LEAST(ROUND(10000*SUM(hits)/SUM(shots))/100,100) AS acc FROM uts_weaponstats WHERE matchid = '0'  GROUP BY weapon") or die(mysql_error());
 	while ($r_weaponstats = mysql_fetch_array($q_weaponstats))
 	{
-		mysql_query("INSERT INTO uts_weaponstats SET matchid='0', pid='0', weapon='$r_weaponstats['weapon']', kills='$r_weaponstats['kills']', shots='$r_weaponstats['shots']', hits='$r_weaponstats['hits']', damage='$r_weaponstats['damage']', acc='$r_weaponstats['acc']'") or die(mysql_error());
+		mysql_query("INSERT INTO uts_weaponstats SET matchid='0', pid='0', weapon='${r_weaponstats['weapon']}', kills='${r_weaponstats['kills']}', shots='${r_weaponstats['shots']}', hits='${r_weaponstats['hits']}', damage='${r_weaponstats['damage']}', acc='${r_weaponstats['acc']}'") or die(mysql_error());
 	}
 
 	echo'<td class="grey" align="left" width="400">Done</td>
